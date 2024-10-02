2efee00cd8cccd414dab7a184742add713ab4ec2
marscripts <mahdi.rahimi@ifp.ag>
2024-10-02 08:51:52 +0200

    Update netzlaufwerk_verbinden.bat

--- batchfiles/netzlaufwerk_verbinden.bat
+++ batchfiles/netzlaufwerk_verbinden.bat
@@ -18,20 +18,11 @@ set PASSWORD_1=deinPasswort1
 
 REM Überprüfen, ob das erste Netzlaufwerk bereits verbunden ist, und ggf. trennen
 if exist %DRIVE_LETTER_1%\ (
-    echo Trenne vorhandenes Netzlaufwerk %DRIVE_LETTER_1%
-    net use %DRIVE_LETTER_1% /delete /yes
+    net use %DRIVE_LETTER_1% /delete /yes >nul 2>&1
 )
 
 REM Erstes Netzlaufwerk verbinden
-echo Verbinde Netzlaufwerk %DRIVE_LETTER_1% mit %NAS_SERVER_1%%SHARE_NAME_1%
-net use %DRIVE_LETTER_1% %NAS_SERVER_1%%SHARE_NAME_1% /user:%USERNAME_1% %PASSWORD_1% /persistent:no
-
-REM Verbindung prüfen für das erste Netzlaufwerk
-if exist %DRIVE_LETTER_1%\ (
-    echo Erstes Netzlaufwerk erfolgreich verbunden.
-) else (
-    echo Fehler beim Verbinden des ersten Netzlaufwerks.
-)
+net use %DRIVE_LETTER_1% %NAS_SERVER_1%%SHARE_NAME_1% /user:%USERNAME_1% %PASSWORD_1% /persistent:no >nul 2>&1
 
 REM ---- Zweite Freigabe ----
 
@@ -50,19 +41,9 @@ set PASSWORD_2=deinPasswort2
 
 REM Überprüfen, ob das zweite Netzlaufwerk bereits verbunden ist, und ggf. trennen
 if exist %DRIVE_LETTER_2%\ (
-    echo Trenne vorhandenes Netzlaufwerk %DRIVE_LETTER_2%
-    net use %DRIVE_LETTER_2% /delete /yes
+    net use %DRIVE_LETTER_2% /delete /yes >nul 2>&1
 )
 
 REM Zweites Netzlaufwerk verbinden
-echo Verbinde Netzlaufwerk %DRIVE_LETTER_2% mit %NAS_SERVER_2%%SHARE_NAME_2%
-net use %DRIVE_LETTER_2% %NAS_SERVER_2%%SHARE_NAME_2% /user:%USERNAME_2% %PASSWORD_2% /persistent:no
+net use %DRIVE_LETTER_2% %NAS_SERVER_2%%SHARE_NAME_2% /user:%USERNAME_2% %PASSWORD_2% /persistent:no >nul 2>&1
 
-REM Verbindung prüfen für das zweite Netzlaufwerk
-if exist %DRIVE_LETTER_2%\ (
-    echo Zweites Netzlaufwerk erfolgreich verbunden.
-) else (
-    echo Fehler beim Verbinden des zweiten Netzlaufwerks.
-)
-
-pause
